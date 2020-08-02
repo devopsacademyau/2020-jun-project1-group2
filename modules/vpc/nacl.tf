71b93cefbacf47f2779b948d8e7be1bae570b8cc
Marcos Monteiro de Souza <30852082+mrcsmonteiro@users.noreply.github.com>
2020-08-03 07:59:50 +1000

    Implement network components (#25)

---
+++ modules/vpc/nacl.tf
@@ -0,0 +1,124 @@
+resource "aws_network_acl" "pub_nacl" {
+  subnet_ids = tolist(data.aws_subnet_ids.public.ids)
+  vpc_id     = aws_vpc.vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 130
+    action     = "allow"
+    cidr_block = "10.0.0.0/16"
+    from_port  = 1
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "public-nacl"
+  }
+}
+
+resource "aws_network_acl" "pvt_nacl" {
+  subnet_ids = tolist(data.aws_subnet_ids.private.ids)
+  vpc_id     = aws_vpc.vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.vpc_cidr
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = var.vpc_cidr
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = var.vpc_cidr
+    from_port  = 22
+    to_port    = 22
+  }
+
+  tags = {
+    Name = "private-nacl"
+  }
+}
+
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.vpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.vpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "default-nacl-wordpress"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "pub_sub_id_az_a" {
+  description = "Public subnet ID"
+  value       = module.vpc.pub_sub_id_az_a
+}
+
+output "pub_sub_id_az_b" {
+  description = "Public subnet ID"
+  value       = module.vpc.pub_sub_id_az_b
+}
+
+output "pvt_sub_id_az_a" {
+  description = "Private subnet ID"
+  value       = module.vpc.pvt_sub_id_az_a
+}
+
+output "pvt_sub_id_az_b" {
+  description = "Private subnet ID"
+  value       = module.vpc.pvt_sub_id_az_b
+}
