9388dda225694d91e166032a4f11149fde3058c1
Timothy OBrien <obrien.timothy.a@gmail.com>
2020-08-03 19:12:06 +1000

    clean up project strucuture (#32)
    Co-authored-by: Timothy OBrien <timothy.obrien@bendigoadelaide.com.au>

--- terraform/_variables.tf
+++ terraform/_variables.tf
@@ -10,3 +10,47 @@ variable "ssh_key_name" {
 variable secure_strings {
 
 }
+
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "azs" {
+  type    = list
+  default = ["ap-southeast-2a", "ap-southeast-2b"]
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = map(object({
+    cidr = string
+    az   = string
+  }))
+}
+
+variable "private_subnets" {
+  type = map(object({
+    cidr = string
+    az   = string
+  }))
+}
+
+variable my_ip {
+  type = string
+}
+
+variable ami_id {
+  type = string
+}
+
+variable aws_acct_number {
+  type = string
+}
+
+variable container_name {
+  type = string
+}
