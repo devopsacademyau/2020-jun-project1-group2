bd1030a7aa76109460d3db1634cf8f52f6036102
nimmi89 <nimmiv1991@gmail.com>
2020-08-06 17:37:00 +1000

    Update README,variables

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "define the aws region to deploy the resources"
-  type        = string
-  default     = "ap-southeast-2"
-}
-
 
 variable "scan_images_on_push" {
   type        = bool
@@ -11,10 +5,10 @@ variable "scan_images_on_push" {
   default     = false
 }
 
-variable "image_names" {
-  type        = list(string)
-  default     = []
-  description = "List of Docker local image names, used as repository names for AWS ECR "
+variable "ecr_name" {
+  type        = string
+  default     = wordpress-ecr
+  description = "Name of the ECR repository"
 }
 
 variable "image_tag_mutability" {
@@ -29,8 +23,4 @@ variable "principals_full_access" {
   default     = []
 }
 
-variable "principals_readonly_access" {
-  type        = list(string)
-  description = "Principal ARNs to provide with readonly access to the ECR"
-  default     = []
-}
+
