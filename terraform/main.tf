47416146759f8231ecc3e0e4fc3a6d0a9300e7ef
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-06 18:50:28 +1000

    remove(code): cleanup old code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-# commenting this out
-# it seems like it ingress
-# not finished yet...
-#module "ASG-LB-ECS" {
-#    source = "./modules/ASG-LB-ECS"
-#    cluster_name = "PROVIDE ECS CLUSTER NAME"
-#    security_groups = ["LIST OF SECURITY GROUPS"]
-#    subnet_ids = ["LIST OF SUBNETS IDS"]
-#    vpc_id = "ID OF THE VPC (module.vpc_module.vpc_id_output)"
-#}
-
 module "vpc" {
   source          = "./modules/vpc"
   region          = var.region
--- terraform/modules/ASG-LB-ECS/README.md
+++
@@ -1,8 +0,0 @@
-2020-jun-project1-group2
-
-## Terraform code 
-- [_backend.tf](_backend.tf)
-- [_provider.tf](_provider.tf)
-- [_variables.tf](_variables.tf)
-- [load-balancer.tf](load-balancer.tf)
-- [main.tfvars](main.tfvars)
