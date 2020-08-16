d2a6b0f9f2aef7057cce2e4e7694315d366e0b0f
Timothy OBrien <obrien.timothy.a@gmail.com>
2020-08-16 15:10:40 +1000

    Obrientimothya/feature/ssm module (#61)
    * update ssm module to accept parameters
    
    * Update SSM module. Add build commands to Makefile
    
    Co-authored-by: Timothy OBrien <timothy.obrien@bendigoadelaide.com.au>

--- terraform/modules/ssm/ssm.tf
+++ terraform/modules/ssm/ssm.tf
@@ -1,14 +1,44 @@
-resource "aws_ssm_parameter" "secure_string" {
-  for_each = { for value in var.secure_strings : value => value }
-
-  name        = each.value
-  description = "Secure String ${each.value}"
-  value       = "empty"
+resource "aws_ssm_parameter" "wordpress_db_host" {
+  name        = "/wordpress/WORDPRESS_DB_HOST"
+  description = "Secure String /wordpress/WORDPRESS_DB_HOST"
+  value       = var.wordpress_db_host
   type        = "SecureString"
-  overwrite   = false
-
+  overwrite   = true
   lifecycle {
     ignore_changes = [value]
   }
 }
 
+
+resource "aws_ssm_parameter" "wordpress_db_name" {
+  name        = "/wordpress/WORDPRESS_DB_NAME"
+  description = "Secure String /wordpress/WORDPRESS_DB_NAME"
+  value       = var.wordpress_db_name
+  type        = "SecureString"
+  overwrite   = true
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "wordpress_db_password" {
+  name        = "/wordpress/WORDPRESS_DB_PASSWORD"
+  description = "Secure String /wordpress/WORDPRESS_DB_PASSWORD"
+  value       = "insecurepassword"
+  type        = "SecureString"
+  overwrite   = true
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "wordpress_db_user" {
+  name        = "/wordpress/WORDPRESS_DB_USER"
+  description = "Secure String /wordpress/WORDPRESS_DB_USER"
+  value       = var.wordpress_db_user
+  type        = "SecureString"
+  overwrite   = true
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,9 +1,9 @@
 module "rds" {
   source                 = "./modules/rds"
   rds_cluster_name       = "${var.application_name}-cluster"
-  rds_database_name      = data.aws_ssm_parameter.database_name.value
-  rds_database_user      = data.aws_ssm_parameter.database_user.value
-  rds_database_password  = data.aws_ssm_parameter.database_password.value
+  rds_database_name      = module.ssm.wordpress_db_name
+  rds_database_user      = module.ssm.wordpress_db_user
+  rds_database_password  = module.ssm.wordpress_db_password
   rds_subnet_ids         = [module.vpc.pvt_sub_id_az_a, module.vpc.pvt_sub_id_az_b]
   rds_security_group_ids = [aws_security_group.rds_sg.id]
 }
