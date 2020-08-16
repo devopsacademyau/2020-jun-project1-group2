907f7257637e4cf39642679a26710e11a4e3ae0e
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-16 12:12:07 +1000

    update ami name

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion" {
-  ami                         = "ami-969ab1f6"
+  ami                         = "ami-0ded330691a314693"
   key_name                    = var.key_pair_name
   instance_type               = "t2.micro"
   security_groups             = [aws_security_group.bastion_sg.id]
